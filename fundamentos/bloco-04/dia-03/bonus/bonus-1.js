05974254376ac793a4bd94a5b01997dfa91585da
Lauro Pereira <lauropera01@gmail.com>
2022-04-09 18:01:30 -0300

    adiciona exercicios bloco 4 dia 3

---
+++ fundamentos/bloco-04/dia-03/bonus/bonus-1.js
@@ -0,0 +1,9 @@
+let square = '';
+let n = 5;
+
+for (let index = 0; index < 5; index += 1) {
+  square += '*';
+}
+for (let index = 5; index > 0; index -= 1) {
+  console.log(square);
+}
---
+++ fundamentos/bloco-04/dia-03/bonus/bonus-2.js
@@ -0,0 +1,7 @@
+let triangle = '';
+let n = 5;
+
+for (let index = 0; index < n; index += 1) {
+  triangle += '*';
+  console.log(triangle);
+}
